5400898c86de91e66e33163dd49e00768abe9d97
sherrywilly <sherrywilson521@gmail.com>
2020-07-19 08:08:52 +0530

    initial commit

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from app import views
+from django.http import HttpResponse
+
+
+def about(request):
+    return HttpResponse("about")
+
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('about/',about)
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from app import forms
 # Create your views here.
 def home(request):
-    form = 
-    return render(request,'home.html')
+    form = forms.CrudForm()
+    return render(request,'home.html',{'form':form})
