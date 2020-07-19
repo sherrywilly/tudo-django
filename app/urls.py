6a70b920f33bedbdf7eeb9ddb2bf8a91c1533186
sherrywilly <sherrywilson521@gmail.com>
2020-07-19 12:31:07 +0530

    initial commmit

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,12 @@ from app import views
 from django.http import HttpResponse
 
 
-def about(request):
-    return HttpResponse("about")
+
 
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('about/',about)
+    path('update/<int:crud_id>',views.update,name='update'),
+    path('delete/<int:crud_id>',views.delete,name='delete'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-from app import forms
+from django.shortcuts import render,redirect
+from app.forms import CrudForm
+from app.models import Crud
 # Create your views here.
+
 def home(request):
-    form = forms.CrudForm()
-    return render(request,'home.html',{'form':form})
+    form = CrudForm()
+    cruds = Crud.objects.all()
+    if request.method == 'POST':
+       form = CrudForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect('home')
+    return render(request,'home.html',{'form':form, 'cruds':cruds})
+
+def update(request, crud_id):
+    crud = Crud.objects.get(id=crud_id)
+    form =CrudForm(instance=crud)
+    if request.method =='POST':
+        form =CrudForm(request.POST,instance=crud)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'update.html',{'form':form,'crud':crud})
+
+def delete(request,crud_id):
+     if request.method == 'POST':
+         Crud.objects.get(id=crud_id).delete()
+         return redirect('home')
